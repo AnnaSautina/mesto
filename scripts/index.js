8a64b9ee82444fb3963bc8e7aa10a06635ca4f14
Anna Sautina <sautina.anechka@yandex.ru>
2021-11-15 21:37:42 +0400

    убрать теги svg

--- scripts/index.js
+++ scripts/index.js
@@ -5,16 +5,19 @@ const popupOverlay = popup.querySelector('.popup__overlay');
 
 function showPopup() {
   popup.classList.add('popup_opened');
+  document.documentElement.style.overflow = 'hidden';
+  document.body.scroll = "no";
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
+  document.documentElement.style.overflow = 'auto';
+  document.body.scroll = "yes";
 }
 
 pen.addEventListener('click', showPopup);
 buttonClose.addEventListener('click', closePopup);
 popupOverlay.addEventListener('click', closePopup);
-
 
 let nameUser = document.getElementById('user-name').textContent;
 let activityUser = document.getElementById('user-activity').textContent;
@@ -22,14 +25,12 @@ let inputs = document.querySelectorAll('input');
 const popupElement = document.querySelector('.popup__container');
 
 popupElement.addEventListener('submit', formSubmitHandler);
-
 inputs[0].value = nameUser;
 inputs[1].value = activityUser;
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-
   document.getElementById('user-name').textContent = inputs[0].value;
   document.getElementById('user-activity').textContent = inputs[1].value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }

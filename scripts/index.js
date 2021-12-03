102cc1df0bc46ddfd23b7044df3ce1dc56bc33aa
Anna Sautina <sautina.anechka@yandex.ru>
2021-12-03 13:34:09 +0400

    удаление элементов и лайки

--- scripts/index.js
+++ scripts/index.js
@@ -32,8 +32,16 @@ function createCard(item) {
   itemContainer.querySelector('.element__text').textContent = item.name;
   itemContainer.querySelector('.element__photo').src = item.link;
 
+  let likeButton = itemContainer.querySelector('.element__like');
+  AddLikeHandler(likeButton); //обработчик клика по сердечку
+
+  let deleteButton = itemContainer.querySelector('.element__delete');
+  AddClickHandler(deleteButton); // действия пользователя с кнопкой удаления эл.
+
   return itemContainer; //создали карточку
 }
+
+
 render = () => {
   cardElements.forEach((item) => {
     listCardElement.append(createCard(item)); //добавляем её в DOM
@@ -42,16 +50,40 @@ render = () => {
 render();
 
 
+// Добавление/удаление лайка
+function AddLikeHandler(likeButton) {
+  likeButton.addEventListener('click', function(evt) {
+    evt.target.classList.toggle('element__like_active');
+  });
+}
+
+
+// Манипуляции с кнопкой удаления эл.
+function AddClickHandler(deleteButton) {
+  deleteButton.addEventListener('mouseover', function(evt) { //при наведении мыши
+    evt.target.classList.add('element__delete_hover');
+  });
+  deleteButton.addEventListener('mouseout', function(evt) { //при отведении мыши
+    evt.target.classList.remove('element__delete_hover');
+  });
+  deleteButton.addEventListener('click', function(evt) {
+    const itemContainer = deleteButton.closest('.element');
+    itemContainer.remove();
+  });
+}
+
+
 const editButton = document.querySelector('.profile-info__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 const popupUser = document.querySelector('.popup_user');
 const popupMesto = document.querySelector('.popup_mesto');
 
-function showPopup(popup) { //открываем попапы
+//Открываем попапы
+function ShowPopup(popup) {
   popup.classList.add('popup_opened');
 }
-editButton.addEventListener('click', () => showPopup(popupUser)); //popupUser передаём как аргумент переменной popup
-addButton.addEventListener('click', () => showPopup(popupMesto));
+editButton.addEventListener('click', () => ShowPopup(popupUser)); //popupUser передаём как аргумент переменной popup
+addButton.addEventListener('click', () => ShowPopup(popupMesto));
 
 
 let nameUser = document.getElementsByClassName('profile-info__name')[0];
@@ -70,66 +102,39 @@ const popupOverlayUser = popupUser.querySelector('.popup__overlay');
 const buttonCloseMesto = popupMesto.querySelector('.popup__to-close');
 const popupOverlayMesto = popupMesto.querySelector('.popup__overlay');
 
-function closePopup(popup) { // закрываем попапы
+// Закрываем попапы
+function ClosePopup(popup) {
   popup.classList.remove('popup_opened');
 }
-buttonCloseUser.addEventListener('click', () => closePopup(popupUser));
-popupOverlayUser.addEventListener('click', () => closePopup(popupUser));
-buttonCloseMesto.addEventListener('click', () => closePopup(popupMesto));
-popupOverlayMesto.addEventListener('click', () => closePopup(popupMesto));
+buttonCloseUser.addEventListener('click', () => ClosePopup(popupUser));
+popupOverlayUser.addEventListener('click', () => ClosePopup(popupUser));
+buttonCloseMesto.addEventListener('click', () => ClosePopup(popupMesto));
+popupOverlayMesto.addEventListener('click', () => ClosePopup(popupMesto));
 
 
 const userForm = popupUser.querySelector('.popup__form');
 const mestoForm = popupMesto.querySelector('.popup__form');
 
-function userFormSubmitHandler(evt) { //сохраняем введённые данные профиля на странице
+// Сохраняем введённые данные профиля на странице
+function userFormSubmitHandler(evt) {
   evt.preventDefault();
   nameUser.textContent = inputs[0].value;
   activityUser.textContent = inputs[1].value;
-  closePopup(popupUser);
+  ClosePopup(popupUser);
 }
 userForm.addEventListener('submit', userFormSubmitHandler);
 
 
-function createCardSubmitHandler(evt) { //формируем карточку из попапа
+//формируем карточку из попапа
+function createCardSubmitHandler(evt) {
   evt.preventDefault();
+
   let nameMesto = document.getElementById('nameMesto');
   let linkPicture = document.getElementById('linkPicture');
   let card = {name:nameMesto.value, link:linkPicture.value};
   const newCard = createCard(card);
 
   listCardElement.prepend(newCard);
-  closePopup(popupMesto);
+  ClosePopup(popupMesto);
 }
 mestoForm.addEventListener('submit', createCardSubmitHandler); //сохраняем введённые данные места на странице
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const likeButton = itemTemplate.querySelector('.element__like');
-  console.log(likeButton);
-
-function like() {
-  likeButton.addEventListener('click', function(evt){  //клик по сердечку
-    const eventTarget = evt.target;
-    eventTarget.classList.toggle('element__like_active');
-    });
-  return likeButton;
-}
-*/
